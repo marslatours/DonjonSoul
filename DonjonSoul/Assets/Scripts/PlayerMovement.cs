ffd9c0207dcd05ddd9db2654ca0052e8985fd180
Robyn <marslatours@gmail.com>
2022-09-13 10:47:40 +0200

    update

--- DonjonSoul/Assets/Scripts/PlayerMovement.cs
+++ DonjonSoul/Assets/Scripts/PlayerMovement.cs
@@ -8,14 +8,20 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private int speed = 5;
     private Vector2 movement;
     private Rigidbody2D rb;
+    private Animator animator;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
     private void OnMovements (InputValue value)
     {
         movement = value.Get<Vector2>();
+        if (movement.x != 0 || movement.y != 0) { 
+        animator.SetFloat("MoveSpeedX", movement.x);
+        animator.SetFloat("MoveSpeedY", movement.y);
+        }
     }
 
     private void FixedUpdate()
